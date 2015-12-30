dd114bf81973895d98b9ad1887d71cdbe9b61ed9
ryan-wong-- <ryanwong0127@gmail.com>
2015-12-30 17:05:18 +0800

    Signed-off-by: ryan-wong-- <ryanwong0127@gmail.com>

--- 5-Dynamic Programming II/medium-minimum adjustment cost+.java
+++ 5-Dynamic Programming II/medium-minimum adjustment cost+.java
@@ -2,43 +2,41 @@ public class Solution {
     /**
      * @param A: An integer array.
      * @param target: An integer.
+     * 给一个整数数组，调整每个数的大小，使得相邻的两个数的差小于一个给定的整数target，
+     * 调整每个数的代价为调整前后的差的绝对值，求调整代价之和最小是多少。
+     * 注意：每个数都是1~100的正整数
      */
     public int MinAdjustmentCost(ArrayList<Integer> A, int target) {
         // write your code here
-        int len = A.size(), min_;
-        int val = 100;
-        //f[i][j]指到i为止的整个数组，最后一个数调整为j时的最小调整代价
-        int[][] f = new int[len][101];
-        if(len<=1){
+        int cel, floor, min_, adjust, n = A.size(), val = 100;
+        if(n<=1){
             return 0;
         }
-        
-        for(int i=0; i<len; i++){
-            f[i][0] = -1;
+        //f[i][j]是计算到A[i]之后最后一个数调整为j的最小调整代价
+        int[][] f = new int[n][val+1];
+        for(int i=1; i<=val; i++){
+            f[0][i] = Math.abs(A.get(0)-i);
         }
-        for(int j=1; j<=val; j++){
-            f[0][j] = Math.abs(A.get(0)-j);
-        }
-        
-        for(int i=1; i<len; i++){
+        //递推式f[i][j] = min(k){f[i-1][k] + |A(i)-j|}, j-target<=k<=j+target.
+        for(int i=1; i<n; i++){
             for(int j=1; j<=val; j++){
-                //A[i]变成j的代价
-                int delta = Math.abs(A.get(i)-j);
-                //既然已知A[i]要变成j，那么A[i-1]（已经不能变）只能在j+-target之间选
-                int cel = Math.min(j+target, val);
-                int floor = Math.max(j-target, 1);
-                
-                f[i][j] = Integer.MAX_VALUE;
+                cel = Math.min(j+target, val);
+                floor = Math.max(j-target, 1);
+                min_ = Integer.MAX_VALUE;
+                f[i][j] = min_;
+                adjust = Math.abs(A.get(i)-j);
                 for(int k=floor; k<=cel; k++){
-                    //递推：i只需考虑i-1的情况
-                    f[i][j] = Math.min(f[i][j], f[i-1][k]+delta);
+                    min_ = Math.min(min_, f[i-1][k]+adjust);
                 }
+                f[i][j] = min_;
             }
         }
-        min_ = Integer.MAX_VALUE;
+        
+        int result = Integer.MAX_VALUE;
         for(int j=1; j<=val; j++){
-            min_ = Math.min(min_, f[len-1][j]);
+            result = Math.min(result, f[n-1][j]);
         }
-        return min_;
+        
+        return result;
     }
 }
